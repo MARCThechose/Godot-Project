5891efe0051ff5552723ef6bf8d8d84d04f83cda
MARCThechose <iamapersonintheinternet@gmail.com>
2025-12-26 23:05:51 +0800

    car now

--- car-engine/nissan_gtr.gd
+++ car-engine/nissan_gtr.gd
@@ -4,8 +4,13 @@ const MAX_STEER = 0.8
 const ENGINE = 300
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pass
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func process(delta):
+
+func _physics_process(delta): # Added underscore and changed to physics_process
+	# Steering logic
 	steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 2.5)
-	engine_force = Input.get_axis("ui_down", "ui_up") * ENGINE;
+	
+	# Engine logic
+	engine_force = Input.get_axis("ui_down", "ui_up") * ENGINE
